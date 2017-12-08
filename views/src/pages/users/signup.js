ce9881cdb2e7e68457d0982bb3986d6ac477ec59
Samuel Witke <witkesam@gmail.com>
2017-12-08 07:12:09 -0500

    Linkin style login

--- views/src/pages/users/signup.js
+++ views/src/pages/users/signup.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
+import { NavLink} from 'react-router-dom'
 
 class Signup extends Component {
 
  constructor(props) {                                                                                                                          
     super(props);      
     this.state = {username:'',email: '', password: '', firstName: '' , lastName: ''} 
-
     this.handleEmail = this.handleEmail.bind(this);
     this.handlePassword = this.handlePassword.bind(this);
-    this.handleFirstName = this.handleFirstName.bind(this);                                                                                                                                       
+    this.handleFirstName = this.handleFirstName.bind(this); 
     this.handleLastName = this.handleLastName.bind(this);          
-    this.handleUsername = this.handleUsername.bind(this);   
-
+    this.handleUsername = this.handleUsername.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);                                                                                          
   }                                                                                                                                            
                                                                                                                                                
-  handleUsername(event,value) {                                                                                                                        
-   this.setState({username: event.target.value});                                                                                                 
+  handleUsername(event,value) { 
+   this.setState({username: event.target.value});
   }   
-  handlePassword(event,value) {                                                                                                                        
-   this.setState({password: event.target.value});                                                                                                 
+  handlePassword(event,value) {                 
+   this.setState({password: event.target.value});
   }  
-  handleEmail(event,value) {                                                                                                                        
+  handleEmail(event,value) {                  
    this.setState({email: event.target.value});                                                                                                 
   }  
   handleFirstName(event,value) {                                                                                                                        
@@ -54,9 +53,12 @@ class Signup extends Component {
   }                                                                                                                                            
                                                                                                                                                
   render() {                                                                                                                                   
-	console.log("here");
+    const login = (
+        <NavLink to="/login" > Here </NavLink>
+		);
+
     return (                     
-		<div className="container">
+		<div className="container" style={{paddingTop: '3cm'}}>
       <div className="row centered-form text-center">
         <div className="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
           <div className="panel panel-default">
@@ -73,50 +75,42 @@ class Signup extends Component {
               </div>
             </div>
           <div className="row">
-          <div className="col-xs-6 col-sm-6 col-mid-6 col-md-offset-3">
-          <div className="form-group">
-          <label> Password:<input type="text" className="form-control" value={this.state.password} onChange={this.handlePassword} /> </label> 
-          </div>
-          </div>
+            <div className="col-xs-6 col-sm-6 col-mid-6 col-md-offset-3">
+              <div className="form-group">
+                <label> Password:<input type="text" className="form-control" value={this.state.password} onChange={this.handlePassword} /> </label> 
+              </div>
+            </div>
           </div>
           <div className="row">
-          <div className="col-xs-6 col-sm-6 col-mid-6 col-md-offset-3">
-          <div className="form-group">
-        <label>                                                                                                                                
-          Email:                                                                                                                                
-          <input type="text" className="form-control" value={this.state.email} onChange={this.handleEmail} /> </label> 
+            <div className="col-xs-6 col-sm-6 col-mid-6 col-md-offset-3">
+              <div className="form-group">
+              <label>  Email:<input type="text" className="form-control" value={this.state.email} onChange={this.handleEmail} /> </label> 
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-6 col-sm-6 col-mid-6 col-md-offset-3">
+              <div className="form-group">
+                <label> Firstname: <input type="text" className="form-control" value={this.state.firstName} onChange={this.handleFirstName} /> 
+                </label> 
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-6 col-sm-6 col-mid-6 col-md-offset-3">
+              <div className="form-group">
+                <label> LastName:<input type="text" className="form-control" value={this.state.lastName} onChange={this.handleLastName} />
+                </label> 
+              </div>
+            </div>
+          </div>
+          <input type="submit"className="form-control" value="Submit" className="btn btn-info btn-block"/> 
+          </form>                                                                                                                                        <h3 className="panel-title">Have an account Sign in {login}</h3>
         </div>
-          </div>
-          </div>
-        <div className="row">
-          <div className="col-xs-6 col-sm-6 col-mid-6 col-md-offset-3">
-          <div className="form-group">
-        <label>                                                                                                                                
-          Firstname:                                                                                                                                
-          <input type="text" className="form-control" value={this.state.firstName} onChange={this.handleFirstName} />                                                          
-        </label> 
-          </div>
-          </div>
-          </div>
-      <div className="row">
-          <div className="col-xs-6 col-sm-6 col-mid-6 col-md-offset-3">
-          <div className="form-group">
-        <label>                                                                                                                                
-          LastName:                                                                                                                                
-          <input type="text" className="form-control" value={this.state.lastName} onChange={this.handleLastName} />                                                          
-
-        </label> 
       </div>
-          </div>
-          </div>
-      
-        <input type="submit"className="form-control" value="Submit" className="btn btn-info btn-block"/> 
-      </form>                                                                                                                                  
-      </div>
-      </div>
-      </div>
-      </div>
-      </div>
+     </div>
+    </div>
+   </div>
     )                                                                                                                                         
   }                                                                                                                                                                                                                                                                                       
 }  
